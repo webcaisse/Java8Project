7c9ac8433fc37b792ebcba456c47e45ea663431c
abdessalem <belhadj.abdessalem@gmail.com>
2017-01-17 00:43:11 +0100

    second project --> expression Lambda

--- src/fr/solinum/java8/Lambda.java
+++ src/fr/solinum/java8/Lambda.java
@@ -5,40 +5,27 @@ public class Lambda {
 	
 	public static void main (String arggs[]){
 
-		Adding adding = (int a , int b) -> a+b;
+		Operation adding = (int a , int b) -> {
+			return a+b;
+		};
+		Operation sub = (int a , int b) -> a-b;
+
 		
-		Substruction sub = (int a , int b) -> a-b;
 		
-		System.out.println("1 + 3 vaut ="+new Lambda().add(1, 3, adding));
+		System.out.println("1 + 3 vaut ="+ adding.op(1, 3));
 		
-		System.out.println("1 - 3 vaut ="+new Lambda().sub(1, 3, sub));
+		System.out.println("1 - 3 vaut ="+sub.op(1, 3));
 	}
 	
 	/**
-	 * interface Adding
+	 * interface Operation
 	 * @author Belhadj
 	 *
 	 */
-	interface Adding {
-		int some (int a, int b);
+	interface Operation {
+		int op (int a, int b);
 	}
+
 	
-	/**
-	 * interface Substruction
-	 * @author Belhadj
-	 *
-	 */
-	interface Substruction {
-		int sub (int a, int b);
-	}
-	
-	private int add (int a, int b, Adding adding){
-		return adding.some(a, b);
-	}
-	
-	
-	private int sub (int a, int b, Substruction sub){
-		return sub.sub(a, b);
-	}
 	
 }
